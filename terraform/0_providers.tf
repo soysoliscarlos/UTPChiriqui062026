39476f141605d567446c2f2dd92892f628dd9138
Carlos Solis Salazar <carlos@soysoliscarlos.com>
2026-06-19 15:17:31 -0500

    Set key vault purge settings to false and add cognitive account purge configuration

--- terraform/0_providers.tf
+++ terraform/0_providers.tf
@@ -2,7 +2,11 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   features {
     key_vault {
-      purge_soft_delete_on_destroy = true
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = false
+    }
+    cognitive_account {
+      purge_soft_delete_on_destroy = false
     }
   }
 }
